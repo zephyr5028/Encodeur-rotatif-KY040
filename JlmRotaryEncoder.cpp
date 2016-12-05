c547272d0bfc13ec10aaedc2be1fa8e2d641c3ad
zephyr5028 <marguerie.jl@wanadoo.fr>
2016-12-05 18:27:02 +0100

    ajout fichiers de la classe

---
+++ JlmRotaryEncoder.cpp
@@ -0,0 +1,73 @@
+#include "JlmRotaryEncoder.h"
+
+// surcharge constructeur avec timer - broches dt et clk ainsi que sw si besoin
+JlmRotaryEncoder::JlmRotaryEncoder(byte dt, byte clk, byte sw = 0) : m_encoderPinA(dt), m_encoderPinB(clk), m_switchButton(sw),
+  m_A_set(false) , m_B_set(false), m_A_change(false), m_B_change(false), m_rotating(false), m_encoderPos(0),  m_unSwitch(true)
+{
+}
+
+// destrcuteur
+JlmRotaryEncoder::~JlmRotaryEncoder()
+{
+}
+
+//initialisation
+void JlmRotaryEncoder::init()
+{
+  pinMode(m_encoderPinA, INPUT_PULLUP); // utilisation du pullup
+  pinMode(m_encoderPinB, INPUT_PULLUP); // utilisation du pullup
+  if (m_switchButton != 0)   pinMode(m_switchButton, INPUT_PULLUP); // utilisation du pullup
+}
+
+// Interruption sur changement d'état de A
+int JlmRotaryEncoder::encoderA()
+{
+  // debounce
+  if ( m_rotating ) delay (3);  // attendre un petit peut
+  m_rotating = true; //activation de l'anti-rebond
+  // Confirmation du changement
+  if ( digitalRead(m_encoderPinA) != m_A_set )  {
+    m_A_set = !m_A_set;
+
+    if (m_B_change) {
+      m_encoderPos += 1;
+      m_B_change = false;
+    } else
+      m_A_change = true;
+
+    m_rotating = false;  //libération de l'anti-rebond
+  }
+  return m_encoderPos;
+}
+
+// Interruption sur changement d'etat de B
+int JlmRotaryEncoder::encoderB()
+{
+  if ( m_rotating ) delay (3);
+  m_rotating = true;
+  if ( digitalRead(m_encoderPinB) != m_B_set )  {
+    m_B_set = !m_B_set;
+
+    if (m_A_change) {
+      m_encoderPos -= 1;
+      m_A_change = false;
+    } else
+      m_B_change = true;
+
+    m_rotating = false;
+  }
+  return m_encoderPos;
+}
+
+// clear le compteur avec le switch
+void JlmRotaryEncoder::switchClear()
+{
+  if (digitalRead(m_switchButton) == LOW )  delay(20); else m_unSwitch = true;
+  if (digitalRead(m_switchButton) == LOW  and m_unSwitch) {
+    m_encoderPos = 0;
+    m_A_change = false;
+    m_B_change = false;
+    m_unSwitch = false;
+  }
+}
+
---
+++ JlmRotaryEncoder.h
@@ -0,0 +1,52 @@
+/*********************************
+
+   classe JlmRotaryEncoder KY040
+
+   pour arduino uno et nano
+
+   12 2016
+
+ *********************************/
+
+#ifndef JlmRotaryEncoder_h
+#define JlmRotaryEncoder_h
+
+
+#include <Arduino.h>
+
+//#include <avr/interrupt.h>
+//#include "WInterrupts.c"
+
+class JlmRotaryEncoder
+{
+  public:
+
+    JlmRotaryEncoder(byte dt, byte clk, byte sw) ; // constructeur avec broches pour dt , clk et sw
+
+    ~JlmRotaryEncoder(); // destructeur
+
+    void init(); // initialisation
+    int encoderA(); //  Interruption sur changement d'état de A
+    int encoderB(); // Interruption sur changement d'etat de B
+    void switchClear(); // clear compteur avec le switch
+
+  protected:
+
+    byte m_encoderPinA; // right (DT) pin A
+    byte m_encoderPinB; // left (CLK) pin B
+    byte m_switchButton; // switch (SW) pin sw
+     boolean m_unSwitch; // un seul appui à la fois sur sw
+
+    volatile unsigned int m_encoderPos;  // un compteur
+    //  unsigned int lastReportedPos = 1;   // gestion du changement
+    boolean m_rotating;    // gestion de l'anti-rebonds
+
+    // variable pour les routines d'interruption
+    boolean m_A_set;
+    boolean m_B_set;
+    boolean m_A_change;
+    boolean m_B_change;
+
+};
+
+#endif
